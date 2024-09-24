fe895a0e3e9782f485a66996148443ad60b44d62
NursultanMaulen <maulen.n@list.ru>
2024-09-24 16:20:00 +0500

    first

---
+++ task2/test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 1
+	var b float64 = 2.12313
+	c := "ok"
+	var d = true
+	fmt.Printf("%s", a)
+	fmt.Printf("%s", b)
+	fmt.Printf("%s", c)
+	fmt.Printf("%s", d)
+}
